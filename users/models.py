7493f476c5b82d3dda23549dd1555bd44e5eff26
Mohammad Maqsood <dev.maddy.1092@gmail.com>
2023-04-16 00:26:57 +0500

    abc

--- users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-# Create your models here.
-class User(AbstractUser):
-    username = None
-    email = models.EmailField(_('email address'), unique=True)
--- vercel_deployement/urls.py
+++ vercel_deployement/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('users/', include('users.apis.v1.urls')),
+    # path('users/', include('users.apis.v1.urls')),
     path('admin/', admin.site.urls),
 ]
